229908fb4c1cb7413b789fb4de4fdee3cb5f6af9
josemhc <jose_mig.hernandez@uao.edu.co>
2024-12-02 09:05:16 -0500

    Solucion de errores de sintaxis

--- frontend/components/ui/fileUpload.tsx
+++ frontend/components/ui/fileUpload.tsx
@@ -257,7 +257,7 @@ export function GridPattern() {
   const columns = 41;
   const rows = 11;
   return (
-    <div className="flex bg-gray-100 dark:bg-neutral-900 flex-shrink-0 flex-wrap justify-center items-center gap-x-px gap-y-px  scale-105">
+    <div className="flex bg-gray-100 dark:bg-neutral-900 flex-shrink-0 flex-wrap justify-center items-center gap-x-px gap-y-px scale-105">
       {Array.from({ length: rows }).map((_, row) =>
         Array.from({ length: columns }).map((_, col) => {
           const index = row * columns + col;
@@ -267,7 +267,9 @@ export function GridPattern() {
               className={`w-10 h-10 flex flex-shrink-0 rounded-[2px] ${
                 index % 2 === 0
                   ? "bg-gray-50 dark:bg-neutral-950"
-                  : "bg-gray-50 dark:bg-neutral-950 shadow-[0px_0px_1px_3px_rgba(255,255,255,1)_inset] dark:shadow-[0px_0px_1px_3px_rgba(0,0,0,1)_inset]`
+                  : "bg-gray-50 dark:bg-neutral-950 " +
+                    "shadow-[0px_0px_1px_3px_rgba(255,255,255,1)_inset] " +
+                    "dark:shadow-[0px_0px_1px_3px_rgba(0,0,0,1)_inset]"
               }`}
             />
           );
@@ -276,4 +278,5 @@ export function GridPattern() {
     </div>
   );
 }
+
 
